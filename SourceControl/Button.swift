cb2824fbf89d8c92130f6d54eadae377cdc2b0b3
Artur <a.nozhenko@ukma.edu.ua>
2024-05-06 15:36:31 +0300

    ViewController updates (progress)

--- SourceControl/Button.swift
+++ SourceControl/Button.swift
@@ -12,11 +12,12 @@ class Button: UIButton {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
     
 }
--- SourceControl/ViewController.swift
+++ SourceControl/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let button = Button()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
