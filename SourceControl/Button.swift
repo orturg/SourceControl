b43c4765d982c7ea8481ab8703edf975f98d7c27
Artur <a.nozhenko@ukma.edu.ua>
2024-05-06 15:35:03 +0300

    Add button

---
+++ SourceControl/Button.swift
@@ -0,0 +1,22 @@
+//
+//  Button.swift
+//  SourceControl
+//
+//  Created by Artur Nozhenko on 06.05.2024.
+//
+
+import UIKit
+
+class Button: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
