4a9728024e4f50e33ba74d25198c8d2c506b1a90
Artur <a.nozhenko@ukma.edu.ua>
2024-05-08 08:36:15 +0300

    [Feature] Add button 2 text color

--- SourceControl/ViewController.swift
+++ SourceControl/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
         
         button2.translatesAutoresizingMaskIntoConstraints = false
         button2.titleLabel?.text = "Button 2"
+        button2.titleLabel?.textColor = UIColor.green
         
         NSLayoutConstraint.activate([
             button.topAnchor.constraint(equalTo: view.topAnchor, constant: 10)
