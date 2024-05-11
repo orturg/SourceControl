909c4060c2a4546ad3407f4a9a54415efebcf5ad
Artur <a.nozhenko@ukma.edu.ua>
2024-05-11 08:25:19 +0300

    [Feature] Change button title

--- SourceControl/ViewController.swift
+++ SourceControl/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         view.backgroundColor = .systemBackground
         view.addSubview(button)
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.titleLabel?.text = "Title11111"
+        button.titleLabel?.text = "Test"
         button.titleLabel?.textColor = UIColor.lightGray
         
         button2.translatesAutoresizingMaskIntoConstraints = false
