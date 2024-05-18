20f6c912c83d13d56ee4f5cda713f514e0c69b12
Artur <a.nozhenko@ukma.edu.ua>
2024-05-18 10:34:35 +0300

    [Feature] Add NewButton background color

--- SourceControl/NewButton.swift
+++ SourceControl/NewButton.swift
@@ -9,5 +9,12 @@ import UIKit
 
 class NewButton: UIButton {
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .black
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
